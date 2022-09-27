57ed2d82b000198e863b5643f8b663ed7bf12215
Lauren <laurencutler@arizona.edu>
2022-09-27 08:18:44 -0700

    class script

--- air_pressure.py
+++ air_pressure.py
@@ -1,16 +1,18 @@
 #code to calculate air pressure at a given height
+#%%
 
 import numpy as np
-
-p0 = 101325 ###Reference Pressure in Pa
-g = 9.81 ###gravity m/s2
-T0 = 288.16 ####Sea level temperature in K
-m = 0.02896968 ###molar mass of air in kg/mol
-R0 = 8.3144 ###universal gas constant in J/(mol*K)
-z = np.array(0,2000,100)
-
-p =  np.zeros((len(z)))  ### height in meters
-for i in range(len(z)):
-    p[i] = p0*np.exp((-g*z[i]*m)/(T0*R0))
-
-print(p)
+import math
+#%%
+def air_pressure(z):
+    p0 = 101325 ###Reference Pressure in Pa
+    g = 9.81 ###gravity m/s2
+    T0 = 288.16 ####Sea level temperature in K
+    m = 0.02896968 ###molar mass of air in kg/mol
+    R0 = 8.3144 ###universal gas constant in J/(mol*K)
+    p = p0*np.exp((-g*z*m)/(T0*R0))
+    print(p)
+    return p
+#%%
+air_pressure(10)
+# %%
